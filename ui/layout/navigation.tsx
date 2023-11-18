a76fdcd43a43bf93f2847f9b3b4b61cfb57530d7
Barto Molina <bartomolina@gmail.com>
2023-11-18 10:02:27 +0300

    fix: fixed bottom navigation

--- ui/layout/navigation.tsx
+++ ui/layout/navigation.tsx
@@ -3,34 +3,44 @@
 import { Gear, Storefront } from "@phosphor-icons/react";
 import { Icon, Tabbar, TabbarLink } from "konsta/react";
 import { useRouter } from "next/navigation";
+import { useContext } from "react";
+
+import { useProfile } from "@/hooks";
+import { RavenContext } from "@/lib/raven-context";
 
 export function Navigation({ activeTab }: { activeTab: string }) {
   const router = useRouter();
+  const { profile } = useContext(RavenContext);
+  const { data: lensProfile } = useProfile();
 
   return (
     <Tabbar className="fixed bottom-0 left-0 h-24 pt-6">
-      <TabbarLink
-        active={activeTab === "restaurants"}
-        onClick={() => router.push("")}
-        icon={
-          <Icon
-            ios={<Storefront size={28} />}
-            material={<Storefront size={28} />}
-          />
-        }
-        label={"Restaurants"}
-      />
-      <TabbarLink
-        active={activeTab === "restaurant"}
-        onClick={() => router.push("/restaurant")}
-        icon={
-          <Icon
-            ios={<Storefront size={28} />}
-            material={<Storefront size={28} />}
-          />
-        }
-        label={"Restaurant"}
-      />
+      {profile && profile.type === "user" && (
+        <TabbarLink
+          active={activeTab === "restaurants"}
+          onClick={() => router.push("")}
+          icon={
+            <Icon
+              ios={<Storefront size={28} />}
+              material={<Storefront size={28} />}
+            />
+          }
+          label={"Restaurants"}
+        />
+      )}
+      {profile && profile.type === "restaurant" && lensProfile && (
+        <TabbarLink
+          active={activeTab === "restaurant"}
+          onClick={() => router.push(`/restaurant/${lensProfile.id}`)}
+          icon={
+            <Icon
+              ios={<Storefront size={28} />}
+              material={<Storefront size={28} />}
+            />
+          }
+          label={"Restaurant"}
+        />
+      )}
       <TabbarLink
         active={activeTab === "settings"}
         onClick={() => router.push("/settings")}
